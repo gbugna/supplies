e0c921388d867d64063ae73dbbb0a6eeeda4e99b
Gustavo <gbugna@gmail.com>
2021-04-05 15:59:05 -0300

    broken react table

--- src/components/Insumos.jsx
+++ src/components/Insumos.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, Spinner, Container, Flex, IconButton } from '@chakra-ui/react'
 import { FaPen } from 'react-icons/fa'
+import { MaterialTable, tableIcons, handleRowUpdate, handleRowAdd, handleRowDelete } from 'material-table'
+
+
+
+
 function Insumos() {
   const [error, setError] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false)
@@ -37,16 +42,27 @@ function Insumos() {
       </Flex>
     )
   } else {
+
+    const columns = [
+
+      { title: "Modelo", field: "modelo" },
+      { title: "Fabricante", field: "marca" },
+      { title: "Equipo", field: "equipo" }
+    ]
+
+    console.log({ items });
+
     return (
 
       <Container maxW='container.md'>
-        <Table size='sm' colorScheme='facebook'>
+        {/* <Table size='sm' colorScheme='facebook'>
           <TableCaption placement='top'>Insumos</TableCaption>
           <Thead>
             <Tr>
               <Th>Modelo</Th>
               <Th>Fabricante</Th>
               <Th>Equipo</Th>
+              <Th>Editar</Th>
             </Tr>
           </Thead>
           <Tbody>
@@ -60,7 +76,27 @@ function Insumos() {
             ))}
           </Tbody>
           <Tfoot />
-        </Table>
+        </Table> */}
+        <MaterialTable
+          title="User list from API"
+          columns={columns}
+          data={items}
+          icons={tableIcons}
+          editable={{
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve) => {
+                handleRowUpdate(newData, oldData, resolve);
+              }),
+            onRowAdd: (newData) =>
+              new Promise((resolve) => {
+                handleRowAdd(newData, resolve)
+              }),
+            onRowDelete: (oldData) =>
+              new Promise((resolve) => {
+                handleRowDelete(oldData, resolve)
+              }),
+          }}
+        />
       </Container>
 
     )
